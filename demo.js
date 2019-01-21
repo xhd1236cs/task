33c8ec40a7b72af251e14ff210fe2fb40cf56547
xhd1236cs <18211177418@163.com>
2019-01-21 22:35:08 +0800

    task demo.js

---
+++ demo.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+var fs = require('fs');
+
+var dirName = process.argv[2];
+
+fs.mkdirSync("./" + dirName);// mkdir $1
+process.chdir("./" + dirName); // cd $1
+fs.mkdirSync('css'); // mkdir css
+fs.mkdirSync('js') ;// mkdir js
+
+fs.writeFileSync("./index.html","<!DOCTYPE>\n<title>Hello</title>\n<h1>Hi</h1>");
+fs.writeFileSync("css/style.css", "h1{color: red;}");
+fs.writeFileSync("./js/main.js",'var string = "Hello World"\nalert(string)');
+
+process.exit(0);
